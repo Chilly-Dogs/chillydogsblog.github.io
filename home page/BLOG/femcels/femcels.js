f3d3e4c3e71cefae1693e438cecc1e8c51f6538d
Chilly-Dogs <138266092+Chilly-Dogs@users.noreply.github.com>
2023-09-04 17:08:07 -0400

    Add files via upload

---
+++ home page/BLOG/femcels/femcels.js
@@ -0,0 +1,22 @@
+let x = document.getElementById("fed-msg");
+let y = document.getElementById("misa");
+
+function enter1()
+{
+	x.src = "images/medic.png";
+}
+function exit1()
+{
+	x.src = "images/FED.png";
+}
+
+
+
+function enter2()
+{
+	y.src = "images/JACK.png";
+}
+function exit2()
+{
+	y.src = "images/misa.jpg";
+}
