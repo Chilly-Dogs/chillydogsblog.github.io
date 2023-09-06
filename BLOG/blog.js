39dee6be2e91d9b151d80ba75fb2f4ef2daba39d
Chilly-Dogs <138266092+Chilly-Dogs@users.noreply.github.com>
2023-09-05 22:20:05 -0400

    Add files via upload

---
+++ BLOG/blog.js
@@ -0,0 +1,14 @@
+let God = document.getElementById("thoughts-on-God");
+let Femcel = document.getElementById("femcels");
+let Song = document.getElementById("fav-songs");
+
+// When someone clicks on the blog //
+// They get sent to different webpage //
+function GodBlogClick()
+{
+	window.open("thoughts on God/thoughts-on-God.html","_blank");
+}
+function FemcelBlogClick()
+{
+	window.open("femcels/femcels.html","_blank");
+}
